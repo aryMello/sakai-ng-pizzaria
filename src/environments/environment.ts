86e86c34f0e1c2d4b4f5e8237ab6b102ea53ef07
Ana Melo <onlinebea@hotmail.com>
2024-11-13 19:10:25 -0300

    add set-env.js

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
-    apiKey: "AIzaSyDeGKcQkTHUg6LYwj8pUKZ4TZNhHsNVDqQ",
-    authDomain: "pizzariaangularsakai.firebaseapp.com",
-    projectId: "pizzariaangularsakai",
-    storageBucket: "pizzariaangularsakai.firebasestorage.app",
-    messagingSenderId: "301016468669",
-    appId: "1:301016468669:web:196ce7bc99648c09123bd4",
-    measurementId: "G-CVSFF0MD48"
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
   }
 };
 /*
