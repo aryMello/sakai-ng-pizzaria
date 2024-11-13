8754f6d43ca38989d930a1ae2eb36035b62c4f65
Ana Melo <onlinebea@hotmail.com>
2024-11-13 00:30:19 -0300

    commit add true production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDeGKcQkTHUg6LYwj8pUKZ4TZNhHsNVDqQ",
+    authDomain: "pizzariaangularsakai.firebaseapp.com",
+    projectId: "pizzariaangularsakai",
+    storageBucket: "pizzariaangularsakai.firebasestorage.app",
+    messagingSenderId: "301016468669",
+    appId: "1:301016468669:web:196ce7bc99648c09123bd4",
+    measurementId: "G-CVSFF0MD48"
+  }
 };
