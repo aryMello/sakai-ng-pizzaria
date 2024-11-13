302dec662bec12bc81a18fb02ab24f94b944430b
Ana Melo <onlinebea@hotmail.com>
2024-11-13 19:13:20 -0300

    atualiza firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
+    apiKey: "AIzaSyDeGKcQkTHUg6LYwj8pUKZ4TZNhHsNVDqQ",
+    authDomain: "pizzariaangularsakai.firebaseapp.com",
+    projectId: "pizzariaangularsakai",
+    storageBucket: "pizzariaangularsakai.firebasestorage.app",
+    messagingSenderId: "301016468669",
+    appId: "1:301016468669:web:196ce7bc99648c09123bd4",
+    measurementId: "G-CVSFF0MD48"
   }
 };
 /*
