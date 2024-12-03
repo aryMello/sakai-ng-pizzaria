56018eb5dec5766337ef540997e872194de47162
Erick <137801258+erick-henrick@users.noreply.github.com>
2024-12-03 13:11:33 -0300

    tirando a key da api

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCPP3FziQGpt4oHxIf7BiNr0DHWfZu9fyo",
+  apiKey: "",
   authDomain: "pizzariaangular.firebaseapp.com",
   projectId: "pizzariaangular",
   storageBucket: "pizzariaangular.firebasestorage.app",
